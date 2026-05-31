61da0efb69a01f7e2b0050f7c41cdca29817c406
Mohsen Zamani <mohsenzamanist@gmail.com>
2026-05-31 08:42:04 +0100

    Store repetitions in correct format

--- script.mjs
+++ script.mjs
@@ -5,7 +5,7 @@
 // You can't open the index.html file using a file:// URL.
 
 import { getUserIds } from "./common.mjs";
-import { addData, getData } from "./storage.mjs";
+import { addData, clearData, getData } from "./storage.mjs";
 
 const userSelect = document.getElementById("user-select");
 const contentSection = document.getElementById("content-section");
@@ -13,26 +13,17 @@ const form = document.getElementById("topic-form");
 const topicInput = document.getElementById("topic");
 const dateInput = document.getElementById("date");
 const content = document.getElementById("content");
+const clearBtn = document.getElementById("clear-button");
 
 let selectedUser;
 
-function calculateSpaces(date) {
-  const oneWeek = new Date(date);
-  oneWeek.setDate(oneWeek.getDate() + 7);
-
-  const oneMonth = new Date(date);
-  oneMonth.setMonth(oneMonth.getMonth() + 1);
-
-  const threeMonth = new Date(date);
-  threeMonth.setMonth(threeMonth.getMonth() + 1);
-
-  const sixMonth = new Date(date);
-  sixMonth.setMonth(sixMonth.getMonth() + 1);
-
-  const oneYear = new Date(date);
-  oneYear.setFullYear(oneYear.getFullYear() + 1);
-  return [date, oneWeek, oneMonth, threeMonth, sixMonth, oneYear];
-}
+clearBtn.addEventListener("click", function () {
+  if (!selectedUser) {
+    contentSection.innerHTML = "<p>Select a user.</p>";
+    return;
+  }
+  clearData(selectedUser);
+});
 
 form.addEventListener("submit", function (e) {
   e.preventDefault();
@@ -40,8 +31,9 @@ form.addEventListener("submit", function (e) {
   const topic = topicInput.value;
   const date = dateInput.value;
   const repetitionSpaces = calculateSpaces(date);
-  addData(selectedUser, { [topic]: repetitionSpaces });
-  const userAgendas = getData(selectedUser);
+  const repetitionSpacesDates = calculateDates(repetitionSpaces);
+  addData(selectedUser, { [topic]: repetitionSpacesDates });
+
   renderAgendas(userAgendas);
 });
 
@@ -57,16 +49,16 @@ userSelect.addEventListener("change", function (e) {
   content.classList.remove("hidden");
 });
 
-function renderAgendas(agendas) {
-  if (!agendas) {
+function renderAgendas() {
+  const userAgendas = getData(selectedUser);
+  if (!userAgendas) {
     contentSection.textContent = "No agenda to display.";
     return;
   }
-  agendas.map((agenda) => {
-    console.log(Object.keys(agenda)[0]);
-  });
+  userAgendas.forEach((agenda) => console.log(agenda));
 }
 
+// Functions
 function populateUsersDropDown(list) {
   const options = list.map((l) => {
     const option = document.createElement("option");
@@ -80,11 +72,39 @@ function populateUsersDropDown(list) {
   userSelect.append(defaultOption, ...options);
 }
 
-window.addEventListener("DOMContentLoaded", function () {
+function setDefaultInputDate() {
+  const todayDate = new Date().toISOString().split("T")[0];
+  dateInput.value = todayDate;
+}
+
+function calculateSpaces(date) {
+  const spaces = [
+    { days: 7 },
+    { months: 1 },
+    { months: 3 },
+    { months: 6 },
+    { year: 1 },
+  ];
+  return spaces.map((space) => {
+    const base = new Date(date);
+    if (space.days) base.setDate(base.getDate() + space.days);
+    if (space.months) base.setMonth(base.getMonth() + space.months);
+    if (space.year) base.setFullYear(base.getFullYear() + space.year);
+
+    return base;
+  });
+}
+
+function calculateDates(repetitionSpaces) {
+  return repetitionSpaces.map((space) => space.toISOString().split("T")[0]);
+}
+
+document.addEventListener("DOMContentLoaded", function () {
   const users = getUserIds();
   if (users.length === 0) {
     contentSection.textContent = "No user to display.";
     return;
   }
   populateUsersDropDown(users);
+  setDefaultInputDate();
 });
